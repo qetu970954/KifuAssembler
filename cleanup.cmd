56d592def6bf47e3e26c8e01f9008518149d35e1
kartd09427 <fdja1974@gmail.com>
2019-09-20 11:14:26 +0800

    Remove cleanup.cmd and move the action into setup.cmd

--- cleanup.cmd
+++
@@ -1,1 +0,0 @@
-rm resources/expert.json resources/game.json
--- setup.cmd
+++ setup.cmd
@@ -1,2 +1,5 @@
+pytest
+python -m doctest incorporator.py
+rm resources/expert.json resources/game.json
 scrapy crawl expert -o resources/expert.json
 scrapy crawl game -o resources/game.json
