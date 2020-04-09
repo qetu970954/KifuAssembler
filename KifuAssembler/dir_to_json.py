d5ae78cad6f6127d85c855ff4f4490f1907ceb17
Tzu-Yee <fdja1974.cs07g@nctu.edu.tw>
2020-04-09 16:28:02 +0800

    Refactor for readability

--- KifuAssembler/dir_to_json.py
+++ KifuAssembler/dir_to_json.py
@@ -1,8 +1,9 @@
 # Helper script to grab multiple sgf files inside a directory and put them into a single json file.
 # (so it can be "assembled" into a merged sgf)
-# Note that the game outcome is determined by filename.
-# I.e., If the filename ends with BlackWin, the generated json will mark it BWin
-#       If the filename ends with WhiteWin, the generated json will mark it WWin
+
+# Note that the game outcome is determined by the filename if czf flag is set.
+# I.e., If the filename ends with BlackWin, the generated json will mark it BWin.
+#       If the filename ends with WhiteWin, the generated json will mark it WWin.
 
 import pathlib
 import argparse
@@ -11,7 +12,7 @@ import json
 parser = argparse.ArgumentParser()
 parser.add_argument("input_directory", help="The directory that stores sgf files.")
 parser.add_argument("output_directory", help="The output directory that stores the json file.")
-parser.add_argument("-czf", action='store_true', help="Input sgf files are generate by nctu6_compete")
+parser.add_argument("--czf", action='store_true', help="Input sgf files are generate by nctu6_compete")
 parser.add_argument("--name", default="result.json", help="The json output.")
 
 if __name__ == '__main__':
@@ -23,14 +24,14 @@ if __name__ == '__main__':
     for sgf in pathlib.Path(args.input_directory).glob("*.sgf"):
         with sgf.open('r') as f:
             if args.czf:
-                text = sgf.read_text()[:37]
-                text += ";B[jj]"
-                text += sgf.read_text()[37:]
+                text = sgf.read_text()
 
-                if sgf.stem[-8:0] == "BlackWin":
+                if sgf.stem[-8::] == "BlackWin":
                     result.append({"kifu": text, "url": "", "game_result": "BWin"})
-                elif sgf.stem[-8:0] == "WhiteWin":
+                elif sgf.stem[-8::] == "WhiteWin":
                     result.append({"kifu": text, "url": "", "game_result": "WWin"})
+                elif sgf.stem[-4::] == "Draw":
+                    result.append({"kifu": text, "url": "", "game_result": "Draw"})
             else:
                 result.append({"kifu": sgf.read_text(), "url": "", "game_result": "None"})
 
